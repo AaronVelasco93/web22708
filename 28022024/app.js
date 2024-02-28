583768c1ca1f9573a0a526cfa7a8f7833d74b6b6
AaronVelasco93 <aaronvelasco1993@outlook.com>
2024-02-28 10:47:05 -0600

    Clase 28/02/2024

---
+++ 28022024/app.js
@@ -0,0 +1,24 @@
+//Operadores Relacionales
+let resultado= 10>9;
+console.log(resultado);
+
+let resultado2 = 10 === 10;
+console.log(resultado2);
+
+let resultado3 = 5 !== 9;
+console.log(resultado3);
+
+//Operadores Logicos
+
+let resultadoLogico= true && true;
+let resultadoLogico2 = 20>10 && 51 ==55;
+
+console.log("And "+resultadoLogico);
+console.log("AND 2 "+resultadoLogico2);
+/*
+&& -> Si los dos son verdaderos devuelve un verdadero
+|| -> Con que uno sea verdadero devolvera verdadero
+! -> negacion
+*/
+
+
